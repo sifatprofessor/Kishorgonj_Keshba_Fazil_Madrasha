09377a3bfed1f0cf20d5bb448003ce5315fa800e
sifat-99 <mdabdurrahmansifat@gmail.com>
2024-10-18 19:03:10 +0600

    update the routes for better optimization

--- utils/constant.ts
+++ utils/constant.ts
@@ -1,2 +1,2 @@
-// export const BaseURL = "https://kkfm.vercel.app/";
-export const BaseURL = "http://localhost:3000";
+export const BaseURL = "https://kkfm.vercel.app/";
+// export const BaseURL = "http://localhost:3000";
